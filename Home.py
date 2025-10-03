03f87d43b862d711cfbbbbcf62b6def40c66af7f
salstq <83420789+salstq@users.noreply.github.com>
2025-10-03 17:15:37 +0700

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title = 'Aplikasi Pengolahan Citra', layout='wide')
+st.title("Selamat datang di Aplikasi Pengolahan Citra")
+st.write("""
+Aplikasi ini dibuat guna menyelesaikan tugas mata kuliah Pengolahan Citra.
+Aplikasi ini memungkinkan pengguna untuk:
+- Mengupload gambar
+- Melihat ukuran gambar
+- Melihat RGB setiap pixel gambar
+
+Gunakan menu sidebar untuk berpindah ke halaman lain.
+""")
